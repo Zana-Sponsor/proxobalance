f02c5fb39c009b4ea583f505e26775d7e45a545f
Zana-Sponsor <japanzana403@gmail.com>
2026-09-14 23:22:31 +0300

    Fix weekly results theme import

--- best_metrics_screen.dart
+++ best_metrics_screen.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:solar_iconkit/solar_iconkit.dart';
 
-import '../main.dart' show kAppFont;
+import '../theme/app_theme.dart' show kAppFont;
 import '../services/ad_categories.dart';
 import '../services/best_metrics_service.dart';
 import '../widgets/best_metric_card.dart';
